ae6fbb9404a718ce7e07535a48c7d8ada1c5ef51
itcyrichy <62337448+itcyrichy@users.noreply.github.com>
2021-11-15 04:15:18 +0300

    Initial commit

---
+++ ultra_light.py
@@ -0,0 +1,27 @@
+class Animal:
+    def __init__(self,kind,weight,age,food):
+        self.kind = kind
+        self.weight = weight
+        self.age = age
+        self.__food = food
+    def __str__(self):
+        return f'Вид: {self.kind}, вес: {self.weight}, возраст: {self.age}'
+    def count_some_index(self):
+        return self.weight*self.age/2*100
+
+
+Bob = Animal('cat',80,20,'meat')
+print(Bob.count_some_index())
+print(Bob)
+# print(Bob.__food)
+
+class Bird(Animal):
+    def __init__(self,kind,weight,age,food,size):
+        super().__init__(self,kind,weight,age,food)
+        self.size=size
+    def count_some_index(self):
+        self.weight * self.size
+
+Lilo = Bird('owl',2,0.5,'mice',3)
+print(Bird)
+print(Bird.count_some_index())
